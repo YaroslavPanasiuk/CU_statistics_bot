51f8c4276d658917f27a001869a0af63033d18b8
YaroslavPanasiuk <yaroslav.panasiuk@lnu.edu.ua>
2026-03-05 18:05:36 +0200

    fixed impossible registration

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,5 +3,5 @@ from . import admin, common
 
 def register_handlers(dp: Dispatcher):
     dp.include_router(common.router)
-    dp.include_router(common.register_router)
+    dp.include_router(common.registered_router)
     dp.include_router(admin.router)
