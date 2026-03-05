9fc3b205fb389d5f13ced043219ac0bd182835d4
YaroslavPanasiuk <yaroslav.panasiuk@lnu.edu.ua>
2026-03-05 18:00:18 +0200

    fixed impossible registration

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,6 +2,6 @@ from aiogram import Dispatcher
 from . import admin, common
 
 def register_handlers(dp: Dispatcher):
-    dp.include_router(common.register_router)
     dp.include_router(common.router)
+    dp.include_router(common.register_router)
     dp.include_router(admin.router)
